c3130b7931c014946ee1963964c8c8309662b708
funbunch <shannan_bunch@hotmail.com>
2021-08-21 17:24:20 -0700

    refactored counter

--- 1CounterApp/script.js
+++ 1CounterApp/script.js
@@ -1,16 +1,37 @@
 const count = document.querySelector('.count')
-const sub = document.querySelector('.subtract')
-const resetCount = document.querySelector('.reset')
-const add = document.querySelector('.add')
+const buttons = document.querySelector('.buttons')
+// const sub = document.querySelector('.subtract')
+// const resetCount = document.querySelector('.reset')
+// const add = document.querySelector('.add')
 
-add.addEventListener('click', () => {
-  count.innerHTML++
+
+//refactored event delegation 
+buttons.addEventListener('click', (e) => {
+  if (e.target.classList.contains('add')) count.innerHTML++
+    //console.log("add");
+  if (e.target.classList.contains('subtract')) count.innerHTML--
+  if (e.target.classList.contains('reset')) count.innerHTML = 0
+  setError()
+
 })
 
-sub.addEventListener('click', () => {
-  count.innerHTML--
-})
+function setError() {
+  if (count.innerHTML < 0 ) {
+    count.style.color = 'red'
+  } else {
+    count.style.color = "#fff"
+  }
 
-resetCount.addEventListener('click', () => {
-  count.innerHTML = 0
-})
+}
+
+// add.addEventListener('click', () => {
+//   count.innerHTML++
+// })
+
+// sub.addEventListener('click', () => {
+//   count.innerHTML--
+// })
+
+// resetCount.addEventListener('click', () => {
+//   count.innerHTML = 0
+// })
