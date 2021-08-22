5c836cff66e1a2bb7f32e16a71ec859fe3f61be0
funbunch <shannan_bunch@hotmail.com>
2021-08-22 16:36:32 -0700

    more elegant solution and regex non-alphanuml check

--- 5PalindromeChecker/script.js
+++ 5PalindromeChecker/script.js
@@ -2,18 +2,26 @@ const btn = document.querySelector('.btn')
 const result = document.querySelector('.result')
 
 function palindrome() {
-  const word = document.querySelector('.input-text').value
-  let len = word.length
+  let word = document.querySelector('.input-text').value
+  // let len = word.length
 
-  let start = word.substring(0, Math.floor(len / 2)).toLowerCase()
-  let end = word.substring(len - Math.floor(len / 2)).toLowerCase()
+  word = word.toLowerCase().replace(/[^a-z0-9]+/g, '');
+  
+ if (word === word.split('').reverse().join('')) {
+  result.innerHTML = `${word.toUpperCase()} is a Palindrome`
+ } else {
+  result.innerHTML = `${word.toUpperCase()} is Not a Palindrome`
+ }
 
-  let flip = [...end].reverse().join('')
-  if (start === flip) {
-    result.innerHTML = `${word.toUpperCase()} is a Palindrome`
-  } else {
-    result.innerHTML = `${word.toUpperCase()} is Not a Palindrome`
-  }
+  // let start = word.substring(0, Math.floor(len / 2)).toLowerCase()
+  // let end = word.substring(len - Math.floor(len / 2)).toLowerCase()
+
+  // let flip = [...end].reverse().join('')
+  // if (start === flip) {
+  //   result.innerHTML = `${word.toUpperCase()} is a Palindrome`
+  // } else {
+  //   result.innerHTML = `${word.toUpperCase()} is Not a Palindrome`
+  // }
 
 }
 
